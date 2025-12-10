7be75f8832b7fc075e61c620e5b6d436bb7ea6f4
jboschlagos <jboschlagos@gmail.com>
2025-12-09 23:01:17 -0300

    material de estudio

---
+++ receta.js
@@ -0,0 +1,17 @@
+// Receta: Huevos revueltos
+// 1. Ingredientes (variables)
+let huevos = 3;
+let sal = "una pizca";
+let mantequilla = true;
+
+// 2. Utensilios (funciones)
+function batirHuevos() { /* ... */ }
+function calentarSarten() { /* ... */ }
+
+// 3. Pasos (algoritmo)
+if (mantequilla) {
+    calentarSarten();
+    batirHuevos();
+    // ... resto de pasos
+}
+
